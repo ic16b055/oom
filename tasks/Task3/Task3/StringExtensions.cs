571af3b94f192d72db08b299243c331cbe6cd264
ic16b055 <ic16b055@technikum-wien.at>
2017-03-18 14:33:15 +0100

    .

--- tasks/Task3/Task3/StringExtensions.cs
+++ tasks/Task3/Task3/StringExtensions.cs
@@ -6,7 +6,7 @@ namespace Task3
     {
         /// <summary>
         /// Returns the first maxLength characters of the string,
-        /// or the string itself if it is shorter 
+        /// or the string itself if it is shorter.
         /// </summary>
         public static string Truncate(this string s, int maxLength) => (s == null || s.Length <= maxLength) ? s : s.Substring(0, maxLength);
     }
---
+++ tasks/Task3/Task3/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task3
+{
+    public class Tests
+    {
+        [Test]
+
+        public void SimpleTest()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+    }
+}
